b14e5b3bad8fd440a80281f98b3c0ba7d7d193f8
Alex Curran <alexcurran.va@gmail.com>
2020-09-28 16:22:12 -0500

    Added user and appointment tables

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -1,2 +1,15 @@
 CREATE DATABASE barber_shop;
 
+CREATE TABLE app_user(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE appointment(
+    appointment_id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
+);

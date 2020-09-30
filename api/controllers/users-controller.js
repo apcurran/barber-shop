721020244f2c3a11dad7d74394761b6ca41e4fbb
Alex Curran <alexcurran.va@gmail.com>
2020-09-30 09:44:43 -0500

    Added checks for loginValidation, existing user, and valid password

--- api/controllers/users-controller.js
+++ api/controllers/users-controller.js
@@ -8,7 +8,6 @@ const { signupValidation, loginValidation } = require("../validation/users-valid
 
 // POST controllers
 async function postUserSignup(req, res, next) {
-    // TODO Validate incoming data
     try {
         await signupValidation(req.body);
 
@@ -17,8 +16,8 @@ async function postUserSignup(req, res, next) {
     }
 
     try {
+        // Data valid, now reject creating an existing user.
         const { email, password } = req.body;
-        // Reject creating an existing user.
         const emailExists = await db.query(SQL`
             SELECT app_user.user_id
             FROM app_user
@@ -48,7 +47,35 @@ async function postUserSignup(req, res, next) {
 
 async function postUserLogin(req, res, next) {
     try {
+        await loginValidation(req.body);
 
+    } catch (err) {
+        return res.status(400).json({ error: err.details[0].message });
+    }
+
+    try {
+        // Validation passed, now check for an existing email.
+        const { email, password } = req.body;
+        const userResult = await db.query(SQL`
+            SELECT app_user.user_id
+            FROM app_user
+            WHERE app_user.email = ${email}
+        `);
+        const user = userResult.rows[0];
+
+        if (user.length === 0) {
+            return res.status(400).json({ error: "Email is not found." });
+        }
+
+        // Validate password
+        const validPassword = await bcrypt.compare(password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({ error: "Invalid password." });
+        }
+
+        // Create and assign token
+        
 
     } catch (err) {
         next(err);

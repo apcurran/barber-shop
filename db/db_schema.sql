d4408d0d1e4a3803d337404ac615a1e1b21bae02
Alex Curran <alexcurran.va@gmail.com>
2020-09-30 10:44:36 -0500

    Added appointment date column to appointment table

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE app_user(
 
 CREATE TABLE appointment(
     appointment_id SERIAL PRIMARY KEY,
+    appointment_date TIMESTAMP,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE

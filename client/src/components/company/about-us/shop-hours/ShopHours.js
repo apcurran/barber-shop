7f6c865e47feb9782282d7a49e9af1c9e7383021
Alex Curran <alexcurran.va@gmail.com>
2020-10-13 12:16:51 -0500

    Added hours table and updated styling for position and z-index to create a layered effect

--- client/src/components/company/about-us/shop-hours/ShopHours.js
+++ client/src/components/company/about-us/shop-hours/ShopHours.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import "./ShopHours.css";
+
 function ShopHours() {
     return (
-        <div>
-            Shop Hours
+        <div className="shop-description__hours-table-container">
+            <table className="shop-description__hours-table">
+                <thead className="hours-table__head">
+                    <tr className="hours-table__head__row">
+                        <th className="hours-table__title">Shop Hours</th>
+                    </tr>
+                </thead>
+                <tbody className="hours-table__body">
+
+                </tbody>
+            </table>
         </div>
     )
 }

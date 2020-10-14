60f6102ee396ffb99567e1d354b8a6ce60e27c07
Alex Curran <alexcurran.va@gmail.com>
2020-10-14 15:50:43 -0500

    Adjusted grid layout ordering to reverse img order on even service-container classes

--- client/src/components/company/services/services-list/ServicesList.js
+++ client/src/components/company/services/services-list/ServicesList.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-function ServicesList() {
+import "./ServicesList.css";
+
+function ServicesList({ servicesArr }) {
     return (
-        <div>
-            Services List
-        </div>
+        <main className="services-list-container">
+            {servicesArr.map(service => (
+                <div className="service-container">
+                    <figure className="service__fig">
+                        <img src={service.img_url} alt={service.title} className="service__fig__img" width="640" height="426" />
+                    </figure>
+                    <div className="service__info">
+                        <h2 className="service__info__title">{service.title}</h2>
+                        <span className="service__info__price">${service.price}</span>
+                        <p className="service__info__desc">{service.content}</p>
+                    </div>
+                </div>
+            ))}
+        </main>
     );
 }
 

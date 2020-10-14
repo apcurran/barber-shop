ce49c5b3885b65771fd173f7f2218d2e7ca70913
Alex Curran <alexcurran.va@gmail.com>
2020-10-14 14:07:01 -0500

    Adjusted service table and seeds for db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- employee table
 INSERT INTO employee
     (first_name, last_name, email, skill_level, avatar_url)
 VALUES
@@ -14,3 +15,16 @@ INSERT INTO employee
     (first_name, last_name, email, skill_level, avatar_url)
 VALUES
     ('Ben', 'Smith', 'ben@gmail.com', 2, 'https://randomuser.me/api/portraits/men/15.jpg');
+
+
+-- service table
+INSERT INTO service
+    (title, content, price, img_url)
+VALUES
+    ('Fresh Shave/Beard Trim', 'Mauris vitae magna luctus, interdum dui quis, malesuada enim. Maecenas id odio vitae purus condimentum semper. Quisque porttitor a magna vitae sagittis', 10, 'https://source.unsplash.com/W_wknPS5wjM/640x426');
+
+
+INSERT INTO service
+    (title, content, price, img_url)
+VALUES
+    ('Standard Haircut', 'Sed fermentum, sem sit amet suscipit varius, nunc elit vestibulum tellus, vel mattis magna neque at nibh. Curabitur congue auctor lorem, at commodo turpis malesuada vel. Aenean venenatis porta nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse congue nisi est.', 22, 'https://source.unsplash.com/m4Pd_e-4zKs/640x426');

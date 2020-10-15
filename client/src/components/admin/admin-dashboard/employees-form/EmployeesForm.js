3faaff52597541414bbc82ce40ca352d4488861d
Alex Curran <alexcurran.va@gmail.com>
2020-10-15 15:50:22 -0500

    Memoized callback functions for EmployeesForm component

--- client/src/components/admin/admin-dashboard/employees-form/EmployeesForm.js
+++ client/src/components/admin/admin-dashboard/employees-form/EmployeesForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useDispatch } from "react-redux";
 
 import "./EmployeesForm.css";
@@ -10,6 +10,27 @@ function EmployeesForm({ currentEmployeeData, setIsEditing }) {
     const [updatedEmail, setUpdatedEmail] = useState(currentEmployeeData.email);
     const [updatedSkillLevel, setUpdatedSkillLevel] = useState(currentEmployeeData.skill_level);
     const [updatedAvatarUrl, setUpdatedAvatarUrl] = useState(currentEmployeeData.avatar_url);
+
+    const memSetUpdatedFirstname = useCallback(
+        (event) => setUpdatedFirstName(event.target.value),
+        []
+    );
+    const memSetUpdatedLastName = useCallback(
+        (event) => setUpdatedLastName(event.target.value),
+        []
+    );
+    const memSetUpdatedEmail = useCallback(
+        (event) => setUpdatedEmail(event.target.value),
+        []
+    );
+    const memSetUpdatedSkillLevel = useCallback(
+        (event) => setUpdatedSkillLevel(event.target.value),
+        []
+    );
+    const memSetUpdatedAvatarUrl = useCallback(
+        (event) => setUpdatedAvatarUrl(event.target.value),
+        []
+    );
 
     const dispatch = useDispatch();
 
@@ -33,23 +54,23 @@ function EmployeesForm({ currentEmployeeData, setIsEditing }) {
         <form onSubmit={handleEmployeeSubmit} className="employee-form">
             <div className="employee-form__group">
                 <label htmlFor="first-name" className="employee-form__label">First Name</label>
-                <input type="text" className="employee-form__input" id="first-name" value={updatedFirstName} onChange={(event) => setUpdatedFirstName(event.target.value)} />
+                <input type="text" className="employee-form__input" id="first-name" value={updatedFirstName} onChange={memSetUpdatedFirstname} />
             </div>
             <div className="employee-form__group">
                 <label htmlFor="last-name" className="employee-form__label">Last Name</label>
-                <input type="text" className="employee-form__input" id="last-name" value={updatedLastName} onChange={(event) => setUpdatedLastName(event.target.value)} />
+                <input type="text" className="employee-form__input" id="last-name" value={updatedLastName} onChange={memSetUpdatedLastName} />
             </div>
             <div className="employee-form__group">
                 <label htmlFor="email" className="employee-form__label">Email</label>
-                <input type="email" className="employee-form__input" id="email" value={updatedEmail} onChange={(event) => setUpdatedEmail(event.target.value)} />
+                <input type="email" className="employee-form__input" id="email" value={updatedEmail} onChange={memSetUpdatedEmail} />
             </div>
             <div className="employee-form__group">
                 <label htmlFor="skill-level" className="employee-form__label">Skill Level</label>
-                <input type="number" className="employee-form__input" id="skill-level" value={updatedSkillLevel} onChange={(event) => setUpdatedSkillLevel(event.target.value)} />
+                <input type="number" className="employee-form__input" id="skill-level" value={updatedSkillLevel} onChange={memSetUpdatedSkillLevel} />
             </div>
             <div className="employee-form__group">
                 <label htmlFor="avatar-url" className="employee-form__label">Avatar URL</label>
-                <input type="text" className="employee-form__input" id="avatar-url" value={updatedAvatarUrl} onChange={(event) => setUpdatedAvatarUrl(event.target.value)} />
+                <input type="text" className="employee-form__input" id="avatar-url" value={updatedAvatarUrl} onChange={memSetUpdatedAvatarUrl} />
             </div>
             <button type="submit" className="employee-form__submit">Update</button>
         </form>

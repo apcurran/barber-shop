56ab808a42d6a173151f13adf154bef4bf96d534
Alex Curran <alexcurran.va@gmail.com>
2022-02-18 09:54:36 -0600

    Remove exact from routes and update activeclassname all lowercase

--- client/src/components/admin/admin-dashboard/AdminDashboardLinks.js
+++ client/src/components/admin/admin-dashboard/AdminDashboardLinks.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 function AdminDashboardLinks() {
     return (
         <ul className="admin-dashboard__list">
             <li className="admin-dashboard__item">
-                <NavLink exact to="/admin/dashboard/appointments" activeClassName="admin-dashboard__link--selected" className="admin-dashboard__link">
+                <NavLink to="/admin/dashboard/appointments" activeclassname="admin-dashboard__link--selected" className="admin-dashboard__link">
                     <span>Appointments</span>
                 </NavLink>
             </li>
             <li className="admin-dashboard__item">
-                <NavLink exact to="/admin/dashboard/description" activeClassName="admin-dashboard__link--selected" className="admin-dashboard__link">
+                <NavLink to="/admin/dashboard/description" activeclassname="admin-dashboard__link--selected" className="admin-dashboard__link">
                     <span>Description</span>
                 </NavLink>
             </li>
             <li className="admin-dashboard__item">
-                <NavLink exact to="/admin/dashboard/employees" activeClassName="admin-dashboard__link--selected" className="admin-dashboard__link">
+                <NavLink to="/admin/dashboard/employees" activeclassname="admin-dashboard__link--selected" className="admin-dashboard__link">
                     <span>Employees</span>
                 </NavLink>
             </li>
             <li className="admin-dashboard__item">
-                <NavLink exact to="/admin/dashboard/services" activeClassName="admin-dashboard__link--selected" className="admin-dashboard__link">
+                <NavLink to="/admin/dashboard/services" activeclassname="admin-dashboard__link--selected" className="admin-dashboard__link">
                     <span>Services</span>
                 </NavLink>
             </li>

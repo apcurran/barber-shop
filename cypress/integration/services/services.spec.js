b55b588be5c19f60550e460e8670778c1b8127f6
Alex Curran <alexcurran.va@gmail.com>
2022-02-28 11:50:22 -0600

    Basic test for services page to display all 4 available services

---
+++ cypress/integration/services/services.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("services page", () => {
+    beforeEach(() => {
+        cy.visit("/services");
+    });
+
+    it("displays 4 services", () => {
+        cy.get(".services-list-container")
+            .children()
+            .should("have.length", 4);
+    });
+});

a343d0ebd9d23dd7d1c0951ed67d155a43c70af8
Alex Curran <alexcurran.va@gmail.com>
2022-02-28 13:25:18 -0600

    Log out test

--- cypress/integration/auth/log-in.spec.js
+++ cypress/integration/auth/log-in.spec.js
@@ -14,11 +14,30 @@ describe("log in flow", () => {
 
             cy.get("input[type=password]")
                 .type(Cypress.env("testUserPassword"));
-            
+
             cy.get("button[type=submit]")
                 .click();
 
             cy.url().should("eq", "http://localhost:3000/");
         });
+    });
+
+    it("logs a user out of the application", () => {
+        cy.get("form").within(() => {
+            cy.get("input[type=email]")
+                .type(Cypress.env("testUserEmail"));
+
+            cy.get("input[type=password]")
+                .type(Cypress.env("testUserPassword"));
+
+            cy.get("button[type=submit]")
+                .click();
+        });
+
+        cy.contains(/Log Out/i)
+            .click();
+
+        cy.contains("Log In")
+            .should("exist");
     });
 });

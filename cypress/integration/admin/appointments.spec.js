8b44850560ff6fb5cd902657debf4b9d1c514a43
Alex Curran <alexcurran.va@gmail.com>
2022-03-03 15:40:36 -0600

    Add custom adminLogin func and use adminLogin to replace duplicated log in code in admin appointments.spec.js

--- cypress/integration/admin/appointments.spec.js
+++ cypress/integration/admin/appointments.spec.js
@@ -2,41 +2,32 @@
 
 describe("user book appointment flow", () => {
     beforeEach(() => {
-        cy.visit("/admin/login");
-
-        cy.get("form").within(() => {
-            cy.get("input[type=email]")
-                .type(Cypress.env("adminEmail"));
-
-            cy.get("input[type=password]")
-                .type(Cypress.env("adminPassword"));
-
-            cy.get("button[type=submit]")
-                .click();
-
-            // stub API req
-            cy.intercept("GET", "/api/users/admin/appointments", {
-                statusCode: 200,
-                body: [
-                    {
-                        "appointment_id": 63,
-                        "first_name": "Bob",
-                        "last_name": "Doe",
-                        "phone_number": "555-555-5555",
-                        "created_at": "2022-03-01T22:39:21.000Z",
-                        "user_id": 7
-                    },
-                    {
-                        "appointment_id": 64,
-                        "first_name": "Emily",
-                        "last_name": "Smith",
-                        "phone_number": "777-777-7777",
-                        "created_at": "2022-03-01T22:39:21.000Z",
-                        "user_id": 8
-                    }
-                ]
-            });
+        // stub API req
+        cy.intercept("GET", "/api/users/admin/appointments", {
+            statusCode: 200,
+            body: [
+                {
+                    "appointment_id": 63,
+                    "first_name": "Bob",
+                    "last_name": "Doe",
+                    "phone_number": "555-555-5555",
+                    "created_at": "2022-03-01T22:39:21.000Z",
+                    "user_id": 7
+                },
+                {
+                    "appointment_id": 64,
+                    "first_name": "Emily",
+                    "last_name": "Smith",
+                    "phone_number": "777-777-7777",
+                    "created_at": "2022-03-01T22:39:21.000Z",
+                    "user_id": 8
+                }
+            ]
         });
+        // custom log in func
+        cy.adminLogin();
+
+        cy.visit("/admin/dashboard/appointments");
     });
 
     it("displays a list of 2 user appointments", () => {
@@ -65,7 +56,7 @@ describe("user book appointment flow", () => {
             .last()
             .contains("button", /Done/i)
             .click();
-            
+
         // assert new state
         cy.get(".appointments-list")
             .children()

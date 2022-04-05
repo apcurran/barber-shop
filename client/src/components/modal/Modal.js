0755d81599884e84c523a89c1158fdcee94a0842
Alex Curran <alexcurran.va@gmail.com>
2022-04-05 11:19:37 -0500

    Add motion fade anim to Modal comp

--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import "./Modal.css";
 
 function Modal({ handleBackdropClick, handleCloseBtnClick, children }) {
   return (
-    <div onClick={handleBackdropClick} className="backdrop">
+    <motion.div
+      onClick={handleBackdropClick}
+      className="backdrop"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
       <article className="modal">
         <button onClick={handleCloseBtnClick} className="modal__close-btn" aria-label="Close">
           <svg className="modal__close-btn-icon w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /></svg>
         </button>
         {children}
       </article>
-    </div>
+    </motion.div>
   );
 }
 

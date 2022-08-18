6e732ea91354cb8e2d5c3eabd7149c74ec8434f3
Alex Curran <alexcurran.va@gmail.com>
2022-08-18 15:41:39 -0500

    Add rate-limiting to auth routes

--- api/routes/users-router.js
+++ api/routes/users-router.js
@@ -2,23 +2,32 @@
 
 const express = require("express");
 const router = express.Router();
+const rateLimit = require("express-rate-limit");
 
 const usersController = require("../controllers/users-controller");
 const verifyAuth = require("../middleware/verify-auth");
 const verifyAdmin = require("../middleware/verify-admin");
 
-// APP USER routes
-router.post("/signup", usersController.postUserSignup);
+// Rate-limiting setup
+const authLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 7,
+    message: JSON.stringify({ error: "Too many requests, please try again in a minute." }),
+    legacyHeaders: false
+});
 
-router.post("/login", usersController.postUserLogin);
+// APP USER routes
+router.post("/signup", authLimiter, usersController.postUserSignup);
+
+router.post("/login", authLimiter, usersController.postUserLogin);
 
 // APP USER appointments //
 router.post("/appointments", verifyAuth, usersController.postUserAppointment);
 
 // ADMIN routes
-router.post("/admin/signup", usersController.postAdminSignup);
+router.post("/admin/signup", authLimiter, usersController.postAdminSignup);
 
-router.post("/admin/login", usersController.postAdminLogin);
+router.post("/admin/login", authLimiter, usersController.postAdminLogin);
 
 router.get("/admin/appointments", verifyAuth, verifyAdmin, usersController.getAdminAppointments);
 

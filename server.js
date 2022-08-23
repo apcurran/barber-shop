1787f9b0d360f9f291c7e7f1d5a5f782fffc99cb
Alex Curran <alexcurran.va@gmail.com>
2022-08-23 10:11:12 -0500

    Disable eslint rule to keep fourth param for error handler middleware

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use("/api/users", usersRouter);
 app.use("/api/company", companyRouter);
 
 // General Server Error Handling
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
     console.error(err.message);
 
